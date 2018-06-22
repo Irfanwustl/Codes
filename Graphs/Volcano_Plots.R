62a110bd6d375327ace501e3b89ae3bcc9fbcb3e
Jerome Lin <jerlin@pennmedicine.upenn.edu>
2018-06-22 13:11:44 -0400

    updates

--- Graphs/Volcano_Plots.R
+++ Graphs/Volcano_Plots.R
@@ -8,9 +8,36 @@ results["group"] <- "NotSignificant"
 results[which(results['PValue'] < 0.05 & abs(results['logFC']) < 1.5 ),"group"] <- "Significant"
 results[which(results['PValue'] > 0.05 & abs(results['logFC']) > 1.5 ),"group"] <- "FoldChange"
 results[which(results['PValue'] < 0.05 & abs(results['logFC']) > 1.5 ),"group"] <- "Significant&FoldChange"
-top_peaks <- results[with(results, order(results$logFC, results$PValue)),][1:5,]
-top_peaks <- rbind(top_peaks, results[with(results, order(-results$logFC, results$PValue)),][1:5,])
+top_peaks <- results[with(results, order(results$logFC, results$PValue)),][1:10,]
+top_peaks <- rbind(top_peaks, results[with(results, order(-results$logFC, results$PValue)),][1:10,])
 a <- list()
+results = mutate(results, sig=ifelse(results$PValue<0.05, "FDR<0.05", "Not Sig"))
+p= ggplot(results, aes(logFC, -log10(PValue))) +
++     geom_point(aes(col=group)) +
++     scale_color_manual(values=c("pink", "black","green","red"))
+p+geom_text_repel(data=filter(top_peaks, top_peaks$PValue<0.05), aes(label=X))
+
+
+
+
+
+
+p= ggplot(results, aes(logFC, -log10(PValue))) +
++     geom_point(aes(col=sig)) +
++     scale_color_manual(values=c("red", "black"))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -24,6 +51,6 @@ p+geom_text_repel(data=filter(results, padj<0.05), aes(label=Gene))
 
 
 p = ggplot(results, aes(logFC, -log10(PValue))) +
-+     geom_point(aes(col=sig)) +
-+     scale_color_manual(values=c("red", "green"))
++     geom_point(aes(col=results$group)) +
++     scale_color_manual(values=c("black","red", "green"))
 > p+geom_text_repel(data=filter(top_peaks, top_peaks$PValue<0.05), aes(label=X))
