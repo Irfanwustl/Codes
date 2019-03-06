4216fa512c0589d6e90aa68eab30411636e9bdc2
Jerome Lin <jerlin@pennmedicine.upenn.edu>
2019-03-06 16:19:56 -0500

    update codes

--- Graphs/Volcano_Plots.R
+++ Graphs/Volcano_Plots.R
@@ -2,31 +2,33 @@ library(dplyr)
 library(ggplot2)
 library(ggrepel)
 
-results<-read.table("filename.tsv",h=T)
+results<-read.csv("filename.tsv",h=T)
+results<-results[which(results['padj'] != 'NA'),]
+  results["group"] <- "NotSignificant"
+  results[which(results['padj'] < 0.05 & abs(results['log2FoldChange']) < 1.5 ),"group"] <- "Significant"
+  results[which(results['padj'] > 0.05 & abs(results['log2FoldChange']) > 1.5 ),"group"] <- "FoldChange"
+  results[which(results['padj'] < 0.05 & abs(results['log2FoldChange']) > 1.5 ),"group"] <- "Significant&FoldChange"
+  top_peaks <- results[with(results, order(results$log2FoldChange, results$padj)),][1:10,]
+  top_peaks <- rbind(top_peaks, results[with(results, order(-results$log2FoldChange, results$padj)),][1:10,])
+  a <- list()
+results = mutate(results, sig=ifelse(results$padj<0.05, "padj<0.05", "Not Sig"))
+p= ggplot(results, aes(log2FoldChange, -log10(padj))) +
+     geom_point(aes(col=group)) +
+     scale_color_manual(values=c("pink", "black","green","red"))
+png("volcano_1.png")
+p+geom_text_repel(data=filter(top_peaks, top_peaks$padj<0.05), aes(label=X)) + ggtitle("Volcano Plot of Comparison \n v1 (Left) vs v9 (Right)") +
+  theme(plot.title = element_text(hjust = 0.5))
+dev.off()
 
-results["group"] <- "NotSignificant"
-results[which(results['PValue'] < 0.05 & abs(results['logFC']) < 1.5 ),"group"] <- "Significant"
-results[which(results['PValue'] > 0.05 & abs(results['logFC']) > 1.5 ),"group"] <- "FoldChange"
-results[which(results['PValue'] < 0.05 & abs(results['logFC']) > 1.5 ),"group"] <- "Significant&FoldChange"
-top_peaks <- results[with(results, order(results$logFC, results$PValue)),][1:10,]
-top_peaks <- rbind(top_peaks, results[with(results, order(-results$logFC, results$PValue)),][1:10,])
-a <- list()
-results = mutate(results, sig=ifelse(results$PValue<0.05, "FDR<0.05", "Not Sig"))
-p= ggplot(results, aes(logFC, -log10(PValue))) +
-+     geom_point(aes(col=group)) +
-+     scale_color_manual(values=c("pink", "black","green","red"))
-p+geom_text_repel(data=filter(top_peaks, top_peaks$PValue<0.05), aes(label=X))
 
 
 
 
-
-
-p= ggplot(results, aes(logFC, -log10(PValue))) +
+p= ggplot(results, aes(log2FoldChange, -log10(padj))) +
 +     geom_point(aes(col=sig)) +
 +     scale_color_manual(values=c("red", "black"))
 
-p = ggplot(results, aes(log2FoldChange, -log10(pvalue))) +
+p = ggplot(results, aes(log2FoldChange, -log10(padj))) +
   geom_point(aes(col=group)) +
   scale_color_manual(values=c("col1", "col2","col3","col4"))
 p
@@ -35,7 +37,7 @@ p+geom_text(data=filter(results, padj<0.05), aes(label=Gene))
 p+geom_text_repel(data=filter(results, padj<0.05), aes(label=Gene))
 
 
-p = ggplot(results, aes(logFC, -log10(PValue))) +
-+     geom_point(aes(col=results$group)) +
-+     scale_color_manual(values=c("black","red", "green"))
-> p+geom_text_repel(data=filter(top_peaks, top_peaks$PValue<0.05), aes(label=X))
+p = ggplot(results, aes(log2FoldChange, -log10(padj))) +
+     geom_point(aes(col=results$group)) +
+     scale_color_manual(values=c("black","red", "green"))
+> p+geom_text_repel(data=filter(top_peaks, top_peaks$padj<0.05), aes(label=X))
